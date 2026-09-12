6574b1ad8859282050e0dd3cda445726437f1427
soenneker <jake@soenneker.com>
2026-09-11 19:17:12 -0500

    cleanup

---
+++ test/Soenneker.Blazor.Cloudflare.AiSearch.Demo/Pages/DemoSnippets.cs
@@ -0,0 +1,9 @@
+namespace Soenneker.Blazor.Cloudflare.AiSearch.Demo.Pages;
+
+internal static class DemoSnippets
+{
+    public const string SearchBar = "search-bar";
+    public const string SearchModal = "search-modal";
+    public const string ChatBubble = "chat-bubble";
+    public const string ChatPage = "chat-page";
+}
